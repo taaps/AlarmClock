64d920d1d73e47b6bbcda6c98e95815f204281b8
Tapasvi <tapasvi101@gmail.com>
2019-09-24 14:47:49 -0400

    Added bit organizer functionality for the hour variable.

--- src/bit_organizer.h
+++ src/bit_organizer.h
@@ -13,4 +13,10 @@ void assignMinuteBitA(int number, volatile int * address, int type);
 // Assign the appropriate bits onto the address for the tens digit of the minute variable
 void assignMinuteBitB(int number, volatile int * address, int type);
 
+// Assign the appropriate bits onto the address for the ones digit of the hour variable
+void assignHourBitA(int number, volatile int * address, int type);
+
+// Assign the appropriate bits onto the address for the tens digit of the hour variable
+void assignHourBitB(int number, volatile int * address, int type);
+
 #endif
